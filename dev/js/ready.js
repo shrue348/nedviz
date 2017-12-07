984e2a40674531a47641216ea76b00c4842cadd3
Aleksander Alekseev <shure348@iMac-Aleksander-2.local>
2017-12-08 00:20:13 +0300

    commit

--- dev/js/ready.js
+++ dev/js/ready.js
@@ -40,6 +40,8 @@ $(function(){
 	$('.header_top_car').slick({
 		slidesToShow: 1,
 		dots: true,
+		swipe: true,
+		swipeToSlide: true,
 		arrows: false
 	})
 })
@@ -77,3 +79,29 @@ $(function(){
 	})
 })
 
+$(function(){
+	$('.main_menu__toggle').on('click', function(){
+		$('.main_menu ul').toggleClass('open')
+	});
+
+	$(document).mouseup(function (e){
+		var div = $(".main_menu ul");
+		if (!div.is(e.target)
+		    && div.has(e.target).length === 0) {
+			$('.main_menu ul').removeClass('open')
+		}
+	});
+	$(window).scroll(function(){
+		$('.main_menu ul').removeClass('open')
+	})
+})
+
+
+$(function(){ //плавный скролл к якорям
+	$("a.scrollto, .main_menu ul a").click(function () {
+		var elementClick = $(this).attr("href")
+		var destination = $(elementClick).offset().top;
+		jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 400);
+		return false;
+    });
+});
