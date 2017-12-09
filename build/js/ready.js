52774d2c1b89f634296a0d5c8f02c4efd2c7ef5b
Aleksander Alekseev <shure348@iMac-Aleksander-2.local>
2017-12-09 13:10:35 +0300

    commit 9/12

--- build/js/ready.js
+++ build/js/ready.js
@@ -36,15 +36,32 @@ $(function(){
 	  ]
 	});
 
-
 	$('.header_top_car').slick({
 		slidesToShow: 1,
 		dots: true,
 		swipe: true,
 		swipeToSlide: true,
-		arrows: false
-	})
-})
+		arrows: false,
+		responsive: [
+		{
+	      breakpoint: 980,
+	      settings: {
+	        arrows: true,
+	        centerMode: true,
+	        slidesToShow: 1
+	      }
+	    },
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        arrows: false,
+	        centerMode: true,
+	        slidesToShow: 1
+	      }
+	    }
+	  ]
+	});
+});
 
 
 $(function(){
@@ -61,7 +78,7 @@ $(function(){
 	
 
 $(function(){
-	$('input[name=phone]').mask('+7(999) 999-9999')
+	$('input[name=phone]').mask('+7(999) 999-9999');
 })
 
 $(function(){
@@ -75,13 +92,13 @@ $(function(){
 		}
 	});
 	$('.sect_block .close').on('click', function(){
-		$(this).parent().fadeOut()
+		$(this).parent().fadeOut();
 	})
 })
 
 $(function(){
 	$('.main_menu__toggle').on('click', function(){
-		$('.main_menu ul').toggleClass('open')
+		$('.main_menu ul').toggleClass('open');
 	});
 
 	$(document).mouseup(function (e){
@@ -92,10 +109,9 @@ $(function(){
 		}
 	});
 	$(window).scroll(function(){
-		$('.main_menu ul').removeClass('open')
-	})
-})
-
+		$('.main_menu ul').removeClass('open');
+	});
+});
 
 $(function(){ //плавный скролл к якорям
 	$("a.scrollto, .main_menu ul a").click(function () {
@@ -105,3 +121,73 @@ $(function(){ //плавный скролл к якорям
 		return false;
     });
 });
+
+$(function(){
+	var range = document.getElementById('range');
+	var range_val = document.getElementById('range_val');
+	var range2 = document.getElementById('range2');
+	var range2_val = document.getElementById('range2_val');
+
+	noUiSlider.create(range, {
+		start: [35, 100],
+		connect: true,
+		range: {
+			'min': 35,
+			'max': 100
+		}
+	});
+
+	range.noUiSlider.on('update', function( values, handle ) {
+		var val1 = number_format(values[0], 0, ',', ' ');
+		var val2 = number_format(values[1], 0, ',', ' ');
+
+		val1 = val1 + ' - ' + val2;
+
+		$('#range_val span').text(val1)
+	});
+
+	noUiSlider.create(range2, {
+		start: [3000000],
+		connect: true,
+		step: 500000,
+		range: {
+			'min': 3000000,
+			'max': 10000000
+		}
+	});
+
+	range2.noUiSlider.on('update', function( values, handle ) {
+		var val = number_format(values[0], 0, ',', ' ');
+
+		$('#range2_val span').text(val);
+	});
+});
+
+
+
+function number_format(number, decimals, dec_point, separator ) {
+  number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+  var n = !isFinite(+number) ? 0 : +number,
+    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+    sep = (typeof separator === 'undefined') ? ',' : separator ,
+    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+    s = '',
+    toFixedFix = function(n, prec) {
+      var k = Math.pow(10, prec);
+      return '' + (Math.round(n * k) / k)
+        .toFixed(prec);
+    };
+  // Фиксим баг в IE parseFloat(0.55).toFixed(0) = 0;
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n))
+    .split('.');
+  if (s[0].length > 3) {
+    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+  }
+  if ((s[1] || '')
+    .length < prec) {
+    s[1] = s[1] || '';
+    s[1] += new Array(prec - s[1].length + 1)
+      .join('0');
+  }
+  return s.join(dec);
+}
